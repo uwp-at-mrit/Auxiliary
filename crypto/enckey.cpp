c0fabb7c0228ce0f643178b955a6e96864023209
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-12-23 16:02:57 +0800

    enckey passes the test

--- crypto/enckey.cpp
+++ crypto/enckey.cpp
@@ -1,112 +1,69 @@
 #include "crypto/enckey.hpp"
-#include "crypto/blowfish.hpp"
 
-#include "datum/string.hpp"
+#include "datum/bytes.hpp"
 
 using namespace WarGrey::SCADA;
 
-static Natural inline make_HW_ID(uint64 l0000, uint64 l000, uint64 l00, uint64 l0, uint64 l) {
-	return Natural((l0000 << 32U) ^ (l000 << 24U) ^ (l00 << 16U) ^ (l0 << 8U) ^ l);
-}
-
-static Natural inline make_HW_ID(const uint8* id, size_t start) {
-	return make_HW_ID(
-		hexadecimal_ref(id, start, 0U),
-		hexadecimal_ref(id, start + 1, 0U),
-		hexadecimal_ref(id, start + 2, 0U),
-		hexadecimal_ref(id, start + 3, 0U),
-		hexadecimal_ref(id, start + 4, 0U));
-}
-
+/**************************************************************************************************/
 template<typename B>
-static Natural ascii_to_hexadecimal(B* ascii, size_t digit_count, size_t start) {
-	size_t end = start + digit_count * 2U;
+static Natural bytes_to_natural(B* bs, size_t digit_count, size_t start) {
+	size_t end = start + digit_count;
 	Natural id(0U);
 
-	for (size_t idx = start; idx < end; idx += 2U) {
-		uint8 digit = ((uint8)(ascii[idx]) - '0') * 10 + ((uint8)(ascii[idx + 1]) - '0');
+	for (size_t idx = 0; idx < digit_count; idx++) {
+		uint8 digit = byte_to_hexadecimal(bs[start + idx], 0U);
 
-		if (digit > 40U) {
-			digit--;
-		}
-
-		id = ((id << 8U) ^ (digit - 30U));
+		id <<= 8U;
+		id += hexadecimal_to_byte(digit);
 	}
 
 	return id;
 }
 
 /**************************************************************************************************/
-Natural WarGrey::SCADA::enc_natural(uint32 literal_id) {
-	uint64 l0000 = (literal_id >> 16U) & 0xFU;
-	uint64 l000 = (literal_id >> 12U) & 0xFU;
-	uint64 l00 = (literal_id >> 8U) & 0xFU;
-	uint64 l0 = (literal_id >> 4U) & 0xFU;
-	uint64 l = literal_id & 0xFU;
+Natural WarGrey::SCADA::enc_natural(uint64 literal_id) {
+	Natural n;
+	uint8 idx = 0;
 
-	return make_HW_ID(
-		(literal_id >> 16U) & 0xFU,
-		(literal_id >> 12U) & 0xFU,
-		(literal_id >> 8U) & 0xFU,
-		(literal_id >> 4U) & 0xFU,
-		literal_id & 0xFU);
+	while (literal_id > 0U) {
+		uint8 digit = literal_id & 0xFU;
+		size_t shift = n.length() * 8U;
+
+		n ^= ((uint64)(hexadecimal_to_byte(digit)) << shift);
+
+		literal_id >>= 4U;
+	}
+
+	return n;
 }
 
-Natural WarGrey::SCADA::enc_natural(const char* literal_id, size_t start) {
-	return make_HW_ID((uint8*)literal_id, start);
+Natural WarGrey::SCADA::enc_natural(const char* literal_id, size_t digit_count, size_t start) {
+	return bytes_to_natural(literal_id, digit_count, start);
 }
 
 Natural WarGrey::SCADA::enc_natural(bytes& literal_id, size_t start) {
-	return make_HW_ID(literal_id.c_str(), start);
+	return bytes_to_natural(literal_id.c_str(), literal_id.size() - start, start);
 }
 
 Natural WarGrey::SCADA::enc_natural(std::string& literal_id, size_t start) {
-	return enc_natural(literal_id.c_str(), start);
+	return bytes_to_natural(literal_id.c_str(), literal_id.size() - start, start);
 }
 
 /**************************************************************************************************/
-bytes WarGrey::SCADA::enc_ascii(uint64 id, size_t digit_count) {
-	size_t hexsize = digit_count * 2U;
-	bytes ascii(hexsize, '0');
-
-	for (size_t idx = hexsize; ((idx > 0U) && (id > 0U)); idx -= 2U, id >>= 8U) {
-		uint8 digit = (uint8)(id & 0xFU) + 30U;
-
-		if (digit >= 40U) {
-			digit++;
-		}
-
-		ascii[idx - 2] = digit / 10 + '0';
-		ascii[idx - 1] = digit % 10 + '0';
-	}
-
-	return ascii;
+bytes WarGrey::SCADA::enc_ascii(uint64 id) {
+	return enc_natural(id).to_hexstring();
 }
 
 bytes WarGrey::SCADA::enc_ascii(Natural& id) {
-	size_t hexsize = id.length() * 2U;
-	bytes ascii(hexsize, '0');
-
-	for (size_t idx = 0; idx < id.length(); idx++) {
-		uint8 digit = id[int(idx)] + 30U;
-
-		if (digit >= 40U) {
-			digit++;
-		}
-
-		ascii[idx * 2U + 0U] = (digit / 10 + '0');
-		ascii[idx * 2U + 1U] = (digit % 10 + '0');
-	}
-
-	return ascii;
+	return id.to_hexstring();
 }
 
 Natural WarGrey::SCADA::enc_natural_from_ascii(const char* ascii, size_t digit_count, size_t start) {
-	return ascii_to_hexadecimal(ascii, digit_count, start);
+	return Natural(16U, (const uint8*)ascii, start, start + digit_count * 2U);
 }
 
 Natural WarGrey::SCADA::enc_natural_from_ascii(bytes& literal_id, size_t digit_count, size_t start) {
-	return ascii_to_hexadecimal(literal_id.c_str(), digit_count, start);
+	return Natural(16U, literal_id.c_str(), start, start + digit_count * 2U);
 }
 
 Natural WarGrey::SCADA::enc_natural_from_ascii(std::string& literal_id, size_t digit_count, size_t start) {
@@ -115,16 +72,9 @@ Natural WarGrey::SCADA::enc_natural_from_ascii(std::string& literal_id, size_t d
 
 /**************************************************************************************************/
 Natural WarGrey::SCADA::enc_hardware_uid6(Natural HW_ID) {
-	return (HW_ID << 8) ^ HW_ID[0];
+	return HW_ID[0] ^ (HW_ID << 8U);
 }
 
-Natural WarGrey::SCADA::enc_hardware_uid_encrypt(Natural& HW_ID, const Natural& M_KEY) {
-	BlowfishCipher bf(M_KEY.to_bytes().c_str(), M_KEY.length());
-	
-	return HW_ID;
-}
-
-/**************************************************************************************************/
 Natural WarGrey::SCADA::enc_natural_pad(Natural n) {
 	size_t size = n.length();
 	size_t remainder = size % 8;
@@ -137,6 +87,12 @@ Natural WarGrey::SCADA::enc_natural_pad(Natural n) {
 			n += padsize;
 		}
 	}
+
+	return n;
+}
+
+Natural WarGrey::SCADA::enc_natural_unpad(Natural n) {
+	n >>= (8U * n[-1]);
 
 	return n;
 }
--- crypto/enckey.hpp
+++ crypto/enckey.hpp
@@ -6,12 +6,12 @@ namespace WarGrey::SCADA {
 	// These APIs are intended to use `Natural` as its primitive data type instead of `uint64`s and `String`s.
 	// Hint: Strings(std::basic_string<unsigned char>) are just base-256 Naturals.
 
-	WarGrey::SCADA::Natural enc_natural(uint32 literal_id);
-	WarGrey::SCADA::Natural enc_natural(const char* literal_id, size_t start = 0U);
+	WarGrey::SCADA::Natural enc_natural(uint64 literal_id);
+	WarGrey::SCADA::Natural enc_natural(const char* literal_id, size_t digit_count, size_t start = 0U);
 	WarGrey::SCADA::Natural enc_natural(bytes& literal_id, size_t start = 0U);
 	WarGrey::SCADA::Natural enc_natural(std::string& literal_id, size_t start = 0U);
 
-	bytes enc_ascii(uint64 id, size_t digit_count = 5U);
+	bytes enc_ascii(uint64 id);
 	bytes enc_ascii(WarGrey::SCADA::Natural& id);
 
 	WarGrey::SCADA::Natural enc_natural_from_ascii(const char* literal_id, size_t digit_count, size_t start = 0U);
@@ -19,8 +19,6 @@ namespace WarGrey::SCADA {
 	WarGrey::SCADA::Natural enc_natural_from_ascii(std::string& literal_id, size_t digit_count, size_t start = 0U);
 
 	WarGrey::SCADA::Natural enc_hardware_uid6(WarGrey::SCADA::Natural HW_ID);
-
 	WarGrey::SCADA::Natural enc_natural_pad(WarGrey::SCADA::Natural bs);
-
-	WarGrey::SCADA::Natural enc_hardware_uid_encrypt(WarGrey::SCADA::Natural& HW_ID, const WarGrey::SCADA::Natural& M_KEY);
+	WarGrey::SCADA::Natural enc_natural_unpad(WarGrey::SCADA::Natural bs);
 }
